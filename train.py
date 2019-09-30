0652de9ac267e05a71f6ed3c3827592537e973f9
bipinkc19 <41479925+bipinkc19@users.noreply.github.com>
2019-09-30 14:23:46 +0545

    Update train.py

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ METHOD = 'try'
 # Loggers
 logdir = "./tensorboard_logs/" + METHOD + "/" + datetime.now().strftime("%Y-%m-%d//%H-%M-%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+tensorboard_callback_collab = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
 
 # Get your datatensors
 image, label = DataLoad('../drive/My Drive/data_test.tfrecords', 32, EPOCHS, BATCHSIZE).return_dataset()
@@ -23,7 +24,7 @@ val_image, val_label = DataLoad('../drive/My Drive/data_test.tfrecords', 32, EPO
 # Combine it with keras
 model_input = keras.layers.Input(tensor=image)
 inception = keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet')
-inception.trainable = True
+inception.trainable = False
 
 average_pooling = keras.layers.GlobalAveragePooling2D()
 
@@ -45,7 +46,7 @@ train_model.compile(optimizer=keras.optimizers.RMSprop(lr=0.0001),
                     target_tensors=[label])
 
 earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=12, verbose=0, mode='min')
-mcp_save = keras.callbacks.ModelCheckpoint('./mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+mcp_save = keras.callbacks.ModelCheckpoint('./model.hd5', save_best_only=True, monitor='val_loss', mode='min')
 reduce_lr_loss = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.15, patience=7, verbose=1, min_delta=1e-4, mode='min')
 
 # Train the model
@@ -53,4 +54,4 @@ train_model.fit(epochs=EPOCHS,
                 steps_per_epoch=STEPS_PER_EPOCH,
                 validation_data=[val_image, val_label],
                 validation_steps=STEPS_PER_EPOCH,
-                callbacks=[tensorboard_callback, earlyStopping, mcp_save, reduce_lr_loss])
+                callbacks=[tensorboard_callback, earlyStopping, mcp_save, reduce_lr_loss, tensorboard_callback_collab])
